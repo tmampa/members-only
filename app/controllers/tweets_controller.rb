da4052b8e9e97230e0540c5cd1412bce4281da3e
tmampa <tmampa94@gmail.com>
2021-07-05 12:07:01 +0200

    Design tweet controller

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,24 @@
+class TweetsController < ApplicationController
+    def index
+        @tweets = Tweet.all
+        end
+       
+        def new
+        @tweet = Tweet.new
+        end
+       
+        def create
+        @tweet = Tweet.new(tweet_params)
+        @tweet.user_id = current_user.id
+        if @tweet.save
+            redirect_to '/tweets#index'
+        else
+          render 'new'
+        end
+        end
+       
+        private
+        def tweet_params
+          params.require(:tweet).permit(:body)
+        end
+end
