a4ef7c1f3fb5f358db3b0dc12ff4ffa7d62e781d
tmampa <tmampa94@gmail.com>
2021-07-05 12:02:55 +0200

    Model association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_05_094433) do
+ActiveRecord::Schema.define(version: 2021_07_05_094603) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
