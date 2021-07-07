172afd06c1b19a8df33253b169e42e8f4aa09923
tmampa <tmampa94@gmail.com>
2021-07-07 17:45:59 +0200

    Delete tweets method

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,25 +1,34 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!
-    def index
-        @tweets = Tweet.all
-        end
-       
-        def new
-        @tweet = Tweet.new
-        end
-       
-        def create
-        @tweet = Tweet.new(tweet_params)
-        @tweet.user_id = current_user.id
-        if @tweet.save
-            redirect_to '/tweets#index'
-        else
-          render 'new'
-        end
-        end
-       
-        private
-        def tweet_params
-          params.require(:tweet).permit(:body)
-        end
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    if @tweet.save
+      redirect_to '/tweets#index'
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+
+    @tweet.destroy
+
+    redirect_to '/', notice: 'Your tweet has been deleted'
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
+  end
 end
