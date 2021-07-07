02771071af118b8d661b5beb2f2279ccf04921aa
tmampa <tmampa94@gmail.com>
2021-07-07 16:11:32 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'tweets#index'
+
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :tweets
+
+  resources :users do
+  end
 end
