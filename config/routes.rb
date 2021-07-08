b828326f2e0edc4dea3a9607a9e00e347fb838c8
tmampa <tmampa94@gmail.com>
2021-07-08 11:19:46 +0200

    Update application routes for tweet resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :tweets
+  resources :tweets, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tweets#index'
 end
